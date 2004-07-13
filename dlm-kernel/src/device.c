31232820adadc6697ea73c88227a0ba2206f148e
Patrick Caulfield <pcaulfie@redhat.com>
2004-07-13 13:51:07 +0000

    Make DLM_LSF_NOCONVGRANT the default for userland lockspaces.

--- dlm-kernel/src/device.c
+++ dlm-kernel/src/device.c
@@ -158,7 +158,7 @@ static int register_lockspace(char *name, struct user_ls **ls)
 
 	status = dlm_new_lockspace((char *)newls->ls_miscinfo.name+strlen(name_prefix)+1,
 				    strlen(newls->ls_miscinfo.name) - strlen(name_prefix) - 1,
-				    &newls->ls_lockspace, 0);
+				    &newls->ls_lockspace, DLM_LSF_NOCONVGRANT);
 
 	if (status != 0) {
 		kfree(newls->ls_miscinfo.name);
