6ed315567dd97d6edd7d5cec6c64c8ddd47343d9
Daniel Phillips <phillips@redhat.com>
2004-10-03 23:26:42 +0000

    Change to named socket for device control connection

--- csnap/service.c
+++ csnap/service.c
@@ -1,22 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
 #include <errno.h> 
 #include <unistd.h> // read
-#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/poll.h>
+#include <sys/un.h>
 #include <netinet/in.h>
-#include "trace.h"
-#include "sock.h" // send_fd, read/writepipe, connect_socket
 #include "../dm-csnap.h" // message codes
 #include "csnap.h" // outbead
+#include "trace.h"
+#include "sock.h" // send_fd, read/writepipe, connect_socket
 
 #define trace trace_on
 
+int accept_client(char *sockname)
+{
+	struct pollfd pollfd = { .events = POLLIN|POLLPRI };
+	struct sockaddr_un addr = { .sun_family = AF_UNIX };
+	int addr_len = sizeof(addr) - sizeof(addr.sun_path) + strlen(sockname);
+	int n, listener = socket(AF_UNIX, SOCK_STREAM, 0);
+
+	strncpy(addr.sun_path, sockname, sizeof(addr.sun_path));
+	if (sockname[0] == '@')
+		addr.sun_path[0] = 0;
+
+	assert(listener > 0);
+	bind(listener, (struct sockaddr *)&addr, addr_len);
+	n = listen(listener, 5);
+	assert(!n);
+
+	pollfd.fd = accept(listener, NULL, NULL);
+	assert(pollfd.fd > 0);
+
+	n = poll(&pollfd, 1, -1);
+	assert(n == 1 && (pollfd.revents & POLLIN) != 0);
+
+	return pollfd.fd;
+}
+
 int monitor(int control, char *host, int port)
 {
-	struct messagebuf message;
 	int server, err;
+	struct messagebuf message;
 
 	while (1) {
 		if ((err = readpipe(control, &message.head, sizeof(message.head))))
@@ -29,71 +54,43 @@ int monitor(int control, char *host, int port)
 		switch (message.head.code) {
 		case NEED_SERVER:
 			server = open_socket(host, port);
-			warn("Client wants a server connection");
 			if (!server)
 				error("Can't connect to %s:%i", host, port);
 			if (outbead(control, CONNECT_SERVER, struct { }) < 0)
 				error("Could not send connect message");
 			if (send_fd(control, server, "fark", 4) < 0)
 				error("Could not pass server connection to target");
-			break;
+			continue;
 		case REPLY_CONNECT_SERVER:
 			warn("Everything connected properly, all is well");
-			break;
+			continue;
 		default: 
 			warn("Unknown message %x", message.head.code);
-			goto pipe_error;
+			continue;
 		}
 	}
-
 message_too_long:
 	warn("message %x too long (%u bytes)\n", message.head.code, message.head.length);
 pipe_error:
-	return -1;
+	return err;
 }
 
 int main(int argc, char *argv[])
 {
-	int sockpair[2], pipepair[2];
-
 	if (argc != 3)
-		error("usage: %s <device> host:port", argv[0]);
+		error("usage: %s sockname host:port", argv[0]);
 
 	char *host = argv[2];
 	int len = strlen(host), port = parse_port(host, &len);
-
 	if (port < 0)
 		error("expected host:port, not %s", host);
 	host[len] = 0;
 
-	if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockpair) == -1)
-		error("Can't create socket pair");
-
 	switch (fork()) {
 	case -1:
 		error("fork failed");
 	case 0: 
-		pipe(pipepair);
-		dup2(pipepair[0], 0);
-		close(pipepair[0]);
-		if (outbead(pipepair[1], CONTROL_SOCKET, struct {int fd;} PACKED, 3) == -1)
-			error("pipe error %i, %s", errno, strerror(errno));
-		close(sockpair[1]);
-		execl("/sbin/dmsetup", "/sbin/dmsetup", "create", "testdev", "test.dm", NULL);
-		error("exec failed, %s", strerror(errno));
+		return monitor(accept_client(argv[1]), host, port);
 	}
-
-	close(sockpair[0]);
-	if (wait(NULL) == -1)
-		error("Device create failed, %s", strerror(errno));
-
-	switch (fork()) {
-	case -1:
-		error("fork failed");
-	case 0: 
-		return monitor(sockpair[1], host, port);
-	}
-
-	warn("monitor started");
 	return 0;
 }
