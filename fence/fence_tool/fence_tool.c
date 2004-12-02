f4683a0c76311a777d0a78bdc0e51e90d1192b3f
David Teigland <teigland@redhat.com>
2004-12-02 10:51:13 +0000

    A bunch of improvements: - Wait for the cluster to be quorate before starting or killing fenced.   Waiting here lets the join/leave be cancelled easily (not the case   if it blocks in SM). - Unfence ourselves before joining the fence domain.  Can be skipped   with -S. - Accept the c/j/f fenced options and pass them on to fenced when we   exec it (since fence_tool is the usual way of starting fenced.) - Get rid of -t timeout option which isn't useful now that we have   the c/j/f options for fenced.

--- fence/fence_tool/fence_tool.c
+++ fence/fence_tool/fence_tool.c
@@ -17,10 +17,14 @@
 #include <stddef.h>
 #include <signal.h>
 #include <string.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <fcntl.h>
 #include <errno.h>
 
+#include "cnxman-socket.h"
 #include "copyright.cf"
 
 #ifndef TRUE
@@ -28,7 +32,7 @@
 #define FALSE 0
 #endif
 
-#define OPTION_STRING			("Vht:")
+#define OPTION_STRING			("VhScj:f:")
 #define LOCKFILE_NAME                   "/var/run/fenced.pid"
 
 #define OP_JOIN  			1
@@ -44,8 +48,12 @@ do \
 while (0)
 
 char *prog_name;
-int operation = 0;
-int wait_time = 0;
+int operation;
+int skip_unfence;
+int cl_sock;
+char our_name[MAX_CLUSTER_MEMBER_NAME_LEN+1];
+
+int dispatch_fence_agent(char *victim, int in);
 
 
 static void lockfile(void)
@@ -69,10 +77,102 @@ static void lockfile(void)
 	close(fd);
 }
 
-static void do_join(void)
+static int setup_sock(void)
 {
-	sleep(wait_time);
-	execlp("fenced", "fenced", NULL);
+	cl_sock = socket(AF_CLUSTER, SOCK_DGRAM, CLPROTO_CLIENT);
+	if (cl_sock < 0)
+		die("cannot create cluster socket %d", cl_sock);
+
+	return 0;
+}
+
+static int self_unfence(void)
+{
+	if (!skip_unfence)
+		dispatch_fence_agent(our_name, 1);
+	return 0;
+}
+
+static int get_our_name(void)
+{
+	struct cl_cluster_node cl_node;
+	int rv;
+
+	memset(&cl_node, 0, sizeof(struct cl_cluster_node));
+
+	rv = ioctl(cl_sock, SIOCCLUSTER_GETNODE, &cl_node);
+	if (rv)
+		die("cannot get node name from cluster");
+
+	memcpy(our_name, cl_node.name, strlen(cl_node.name));
+	return 0;
+}
+
+/*
+ * We wait for the cluster to be quorate in this program because it's easy to
+ * kill this program if we want to quit waiting.  If we just started fenced
+ * without waiting for quorum, fenced's join would then wait for quorum in SM
+ * but we can't kill/cancel it at that point -- we have to wait for it to
+ * complete.
+ *
+ * A second reason to wait for quorum is that the unfencing step involves
+ * cluster.conf lookups through ccs, but ccsd may wait for the cluster to be
+ * quorate before responding to the lookups.  There wouldn't be a problem
+ * blocking there per se, but it's cleaner I think to just wait here first.
+ *
+ * In the case where we're leaving, we want to wait for quorum because if we go
+ * ahead and shut down fenced, the fence domain leave will block in SM where it
+ * will wait for quorum before the leave can be processed.  We can't
+ * kill/cancel the leave at that point, but we can if we're waiting here.
+ *
+ * Waiting here doesn't guarantee we won't end up blocking in SM on the join or
+ * leave, but it avoids it in some common cases which can be helpful.  (Quorum
+ * could easily be lost between the time we wait for it here and then begin the
+ * join/leave process.)
+ */
+
+static int wait_quorum(void)
+{
+	int rv, i = 0;
+
+	while (1) {
+		rv = ioctl(cl_sock, SIOCCLUSTER_ISACTIVE, NULL);
+		if (!rv)
+			die("cluster is not active");
+
+		rv = ioctl(cl_sock, SIOCCLUSTER_ISQUORATE, NULL);
+		if (rv)
+			break;
+
+		sleep(1);
+
+		if (++i > 9 && !(i % 10))
+			printf("waiting for cluster quorum\n");
+	}
+
+	return 0;
+}
+
+static void do_join(int argc, char *argv[])
+{
+	setup_sock();
+	wait_quorum();
+	get_our_name();
+	close(cl_sock);
+	self_unfence();
+
+	/* Options for fenced can be given to this program which then passes
+	   them on to fenced when it's started (now).  We just manipulate the
+	   args for this program a bit before passing them on to fenced.  We
+	   change the program name in argv[0] and remove the "join" which
+	   getopt places as the last argv.
+
+	   Fenced shouldn't barf if it gets any args specific to this program */
+
+	strcpy(argv[0], "fenced");
+	argv[argc - 1] = NULL;
+
+	execvp("fenced", argv);
 	die("starting fenced failed");
 }
 
@@ -84,13 +184,20 @@ static void do_leave(void)
 
 	lockfile();
 
+	/* get the pid of fenced so we can kill it */
+
 	f = fopen(LOCKFILE_NAME, "r");
 	if (!f)
 		die("fenced not running - no file %s", LOCKFILE_NAME);
-
 	fgets(buf, 33, f);
 	sscanf(buf, "%d", &pid);
 	fclose(f);
+
+	setup_sock();
+	wait_quorum();
+	close(cl_sock);
+
+	/* FIXME: check if gfs or clvm are running and exit if so */
 
 	kill(pid, SIGTERM);
 }
@@ -103,9 +210,16 @@ static void print_usage(void)
 	printf("\n");
 	printf("Options:\n");
 	printf("\n");
-	printf("  -t <seconds>     Delay before joining (starting fenced)\n");
 	printf("  -V               Print program version information, then exit\n");
 	printf("  -h               Print this help, then exit\n");
+	printf("  -S               Skip self unfencing on join\n");
+	printf("\n");
+	printf("Fenced options:\n");
+	printf("  these are passed on to fenced when it's started\n");
+	printf("  -c               All nodes are in a clean state to start\n");
+	printf("  -j <secs>        Post-join fencing delay\n");
+	printf("  -f <secs>        Post-fail fencing delay\n");
+	printf("\n");
 }
 
 static void decode_arguments(int argc, char *argv[])
@@ -117,10 +231,6 @@ static void decode_arguments(int argc, char *argv[])
 		optchar = getopt(argc, argv, OPTION_STRING);
 
 		switch (optchar) {
-
-		case 't':
-			wait_time = atoi(optarg);
-			break;
 
 		case 'V':
 			printf("fence_tool %s (built %s %s)\n",
@@ -134,6 +244,10 @@ static void decode_arguments(int argc, char *argv[])
 			exit(EXIT_SUCCESS);
 			break;
 
+		case 'S':
+			skip_unfence = TRUE;
+			break;
+
 		case ':':
 		case '?':
 			fprintf(stderr, "Please use '-h' for usage.\n");
@@ -142,6 +256,12 @@ static void decode_arguments(int argc, char *argv[])
 
 		case EOF:
 			cont = FALSE;
+			break;
+
+		case 'c':
+		case 'j':
+		case 'f':
+			/* Do nothing, just pass these options on to fenced */
 			break;
 
 		default:
@@ -172,7 +292,7 @@ int main(int argc, char *argv[])
 
 	switch (operation) {
 	case OP_JOIN:
-		do_join();
+		do_join(argc, argv);
 		break;
 	case OP_LEAVE:
 		do_leave();
