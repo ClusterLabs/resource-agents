87447c7b3f56ac666ec18d6fa22deb9b795fb919
Ken Preslan <kpreslan@redhat.com>
2005-01-18 16:19:01 +0000

    Unbreak the user tools.

--- gfs/gfs_tool/ondisk.c
+++ gfs/gfs_tool/ondisk.c
@@ -23,6 +23,11 @@
 #define pv(struct, member, fmt) \
 printf("  "#member" = "fmt"\n", struct->member);
 
+#define ENTER(x)
+#define EXIT(x)
+#define RET(x) return
+#define RETURN(x, y) return y
+
 #define WANT_GFS_CONVERSION_FUNCTIONS
 #include <linux/gfs_ondisk.h>
 
