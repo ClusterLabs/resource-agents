e10bd7adb0c9d388cbb8619a9946c64dafd4529f
David Teigland <teigland@redhat.com>
2005-04-20 04:37:28 +0000

    remove print formats we're not using

--- dlm-kernel/src2/dlm_internal.h
+++ dlm-kernel/src2/dlm_internal.h
@@ -56,27 +56,9 @@
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
 #if (BITS_PER_LONG == 64)
-#define PRIu64 "lu"
-#define PRId64 "ld"
-#define PRIo64 "lo"
 #define PRIx64 "lx"
-#define PRIX64 "lX"
-#define SCNu64 "lu"
-#define SCNd64 "ld"
-#define SCNo64 "lo"
-#define SCNx64 "lx"
-#define SCNX64 "lX"
 #else
-#define PRIu64 "Lu"
-#define PRId64 "Ld"
-#define PRIo64 "Lo"
 #define PRIx64 "Lx"
-#define PRIX64 "LX"
-#define SCNu64 "Lu"
-#define SCNd64 "Ld"
-#define SCNo64 "Lo"
-#define SCNx64 "Lx"
-#define SCNX64 "LX"
 #endif
 
 struct dlm_ls;
