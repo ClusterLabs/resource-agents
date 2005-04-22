ca3fb5b1b6b4afc789de0ce5b129a304c8ef04e8
David Teigland <teigland@redhat.com>
2005-04-22 05:42:25 +0000

    update description of dlm.h flags

--- dlm-kernel/src2/dlm.h
+++ dlm-kernel/src2/dlm.h
@@ -52,6 +52,11 @@
  * either return -EAGAIN from the dlm_lock call or will return 0 from
  * dlm_lock and -EAGAIN in the lock status block when the AST is executed.
  *
+ * DLM_LKF_CANCEL
+ *
+ * Used to cancel a pending lock request or conversion.  A converting lock is
+ * returned to its previously granted mode.
+ *
  * DLM_LKF_CONVERT
  *
  * Indicates a lock conversion request.  For conversions the name and namelen
@@ -70,33 +75,25 @@
  * Force a conversion request to be queued, even if it is compatible with
  * the granted modes of other locks on the same resource.
  *
- * DLM_LKF_CANCEL
- *
- * Used to cancel a pending conversion (with dlm_unlock).  Lock is returned to
- * previously granted mode.
- *
  * DLM_LKF_IVVALBLK
  *
- * Invalidate/clear the lock value block.
+ * Invalidate the lock value block.
  *
  * DLM_LKF_CONVDEADLK
  *
- * The granted mode of a lock being converted (from a non-NL mode) can be
- * changed to NL in the process of acquiring the requested mode to avoid
- * conversion deadlock.
+ * Allows the dlm to resolve conversion deadlocks internally by demoting the
+ * granted mode of a converting lock to NL.  The DLM_SBF_DEMOTED flag is
+ * returned for a conversion that's been effected by this.
  *
  * DLM_LKF_PERSISTENT
  *
- * Only relevant to locks originating in userspace. Signals to the ioctl.c code
- * that this lock should not be unlocked when the process exits.
+ * Only relevant to locks originating in userspace.  A persistent lock will not
+ * be removed if the process holding the lock exits.
  *
  * DLM_LKF_NODLKWT
- *
- * This lock is not to be checked for conversion deadlocks.
- *
  * DLM_LKF_NODLCKBLK
  *
- * not yet implemented
+ * net yet implemented
  *
  * DLM_LKF_EXPEDITE
  *
@@ -117,6 +114,20 @@
  *
  * Disregard the standard grant order rules and grant a lock as soon as it
  * is compatible with other granted locks.
+ *
+ * DLM_LKF_ORPHAN
+ *
+ * not yet implemented
+ *
+ * DLM_LKF_ALTPR
+ *
+ * If the requested mode cannot be granted immediately, try to grant the lock
+ * in PR mode instead.  If this alternate mode is granted instead of the
+ * requested mode, DLM_SBF_ALTMODE is returned in the lksb.
+ *
+ * DLM_LKF_ALTCW
+ *
+ * The same as ALTPR, but the alternate mode is CW.
  */
 
 #define DLM_LKF_NOQUEUE        (0x00000001)
