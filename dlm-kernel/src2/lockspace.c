1285108c2267e5d82c7813065f678763cdef2e5d
David Teigland <teigland@redhat.com>
2005-04-27 07:21:42 +0000

    go back to schedule_timeout() instead of msleep() in dlm_scand, not sure what the msleep was doing

--- dlm-kernel/src2/lockspace.c
+++ dlm-kernel/src2/lockspace.c
@@ -46,7 +46,8 @@ int dlm_scand(void *data)
 	while (!kthread_should_stop()) {
 		list_for_each_entry(ls, &lslist, ls_list)
 			dlm_scan_rsbs(ls);
-		ssleep(dlm_config.scan_secs);
+		set_current_state(TASK_INTERRUPTIBLE);
+		schedule_timeout(dlm_config.scan_secs * HZ);
 	}
 	return 0;
 }
