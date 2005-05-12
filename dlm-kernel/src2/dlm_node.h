b08f671a8c086e9e4e886f60b638d75bba0125de
David Teigland <teigland@redhat.com>
2005-05-12 15:08:50 +0000

    no parens around defined values

--- dlm-kernel/src2/dlm_node.h
+++ dlm-kernel/src2/dlm_node.h
@@ -13,13 +13,13 @@
 #ifndef __DLM_NODE_DOT_H__
 #define __DLM_NODE_DOT_H__
 
-#define DLM_ADDR_LEN			(256)
-#define DLM_MAX_ADDR_COUNT		(3)
-#define DLM_NODE_MISC_NAME		("dlm-node")
+#define DLM_ADDR_LEN			256
+#define DLM_MAX_ADDR_COUNT		3
+#define DLM_NODE_MISC_NAME		"dlm-node"
 
-#define DLM_NODE_VERSION_MAJOR		(1)
-#define DLM_NODE_VERSION_MINOR		(0)
-#define DLM_NODE_VERSION_PATCH		(0)
+#define DLM_NODE_VERSION_MAJOR		1
+#define DLM_NODE_VERSION_MINOR		0
+#define DLM_NODE_VERSION_PATCH		0
 
 struct dlm_node_ioctl {
 	__u32	version[3];
@@ -34,7 +34,7 @@ enum {
 	DLM_SET_LOCAL_CMD,
 };
 
-#define DLM_IOCTL			(0xd1)
+#define DLM_IOCTL			0xd1
 
 #define DLM_NODE_VERSION _IOWR(DLM_IOCTL, DLM_NODE_VERSION_CMD, struct dlm_node_ioctl)
 #define DLM_SET_NODE     _IOWR(DLM_IOCTL, DLM_SET_NODE_CMD, struct dlm_node_ioctl)
