538c340aad8e37ee63a5d0764e4d2887d0b62028
David Teigland <teigland@redhat.com>
2005-05-16 07:30:54 +0000

    lib interface for groupd

---
+++ group/lib/libgroup.c
@@ -0,0 +1,209 @@
+/******************************************************************************
+*******************************************************************************
+**
+**  Copyright (C) 2004-2005 Red Hat, Inc.  All rights reserved.
+**
+**  This library is free software; you can redistribute it and/or
+**  modify it under the terms of the GNU Lesser General Public
+**  License as published by the Free Software Foundation; either
+**  version 2 of the License, or (at your option) any later version.
+**
+**  This library is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+**  Lesser General Public License for more details.
+**
+**  You should have received a copy of the GNU Lesser General Public
+**  License along with this library; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**
+*******************************************************************************
+******************************************************************************/
+
+#include <sys/types.h>
+#include <sys/un.h>
+#include <inttypes.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+
+#include "groupd.h"
+#include "libgroup.h"
+
+#define LIBGROUP_MAGIC	0x67727570
+#define MAXARGS		100  /* FIXME */
+#define MAXLINE		256
+
+#define VALIDATE_HANDLE(h) \
+do { \
+	if (!(h) || (h)->magic != LIBGROUP_MAGIC) { \
+		errno = EINVAL; \
+		return -1; \
+	} \
+} while (0)
+
+struct group_handle
+{
+	int magic;
+	int fd;
+	int level;
+	void *private;
+	group_callbacks_t cbs;
+	char prog_name[32];
+};
+
+int group_join(group_handle_t handle, char *name, char *info)
+{
+	char buf[MAXLINE];
+	int rv;
+	struct group_handle *h = (struct group_handle *) handle;
+	VALIDATE_HANDLE(h);
+
+	memset(buf, 0, sizeof(buf));
+	sprintf("join %s", name);
+
+	rv = write(h->fd, buf, strlen(buf));
+
+	return rv;
+}
+
+int group_leave(group_handle_t handle, char *name)
+{
+	char buf[MAXLINE];
+	int rv;
+	struct group_handle *h = (struct group_handle *) handle;
+	VALIDATE_HANDLE(h);
+
+	memset(buf, 0, sizeof(buf));
+	sprintf("leave %s", name);
+
+	rv = write(h->fd, buf, strlen(buf));
+
+	return rv;
+}
+
+int group_done(group_handle_t handle, char *name, int event_nr)
+{
+	char buf[MAXLINE];
+	int rv;
+	struct group_handle *h = (struct group_handle *) handle;
+	VALIDATE_HANDLE(h);
+
+	memset(buf, 0, sizeof(buf));
+	sprintf("done %s %d", name, event_nr);
+
+	rv = write(h->fd, buf, strlen(buf));
+
+	return rv;
+}
+
+group_handle_t group_init(void *private, char *prog_name, int level,
+			  group_callbacks_t *cbs)
+{
+	struct group_handle *h;
+	struct sockaddr_un sun;
+	socklen_t addrlen;
+	char buf[MAXLINE];
+	int rv, saved_errno;
+
+	h = malloc(sizeof(struct group_handle));
+	if (!h)
+		return NULL;
+
+	h->magic = LIBGROUP_MAGIC;
+	h->private = private;
+	h->cbs = *cbs;
+	h->level = level;
+	strncpy(h->prog_name, prog_name, 32);
+
+	h->fd = socket(PF_UNIX, SOCK_STREAM, 0);
+	if (h->fd < 0)
+		goto fail;
+
+	memset(&sun, 0, sizeof(sun));
+	sun.sun_family = AF_UNIX;
+	strcpy(&sun.sun_path[1], GROUPD_SOCK_PATH);
+	addrlen = sizeof(sa_family_t) + strlen(sun.sun_path+1) + 1;
+
+	rv = connect(h->fd, (struct sockaddr *) &sun, addrlen);
+	if (rv < 0)
+		goto fail;
+
+	memset(buf, 0, sizeof(buf));
+	sprintf(buf, "setup %s %d", prog_name, level);
+
+	rv = write(h->fd, &buf, strlen(buf));
+	if (rv < 0)
+		goto fail;
+
+	return (group_handle_t) h;
+
+ fail:
+	saved_errno = errno;
+	close(h->fd);
+	free(h);
+	h = NULL;
+	errno = saved_errno;
+	return NULL;
+}
+
+int group_exit(group_handle_t handle)
+{
+	struct group_handle *h = (struct group_handle *) handle;
+	VALIDATE_HANDLE(h);
+	h->magic = 0;
+	close(h->fd);
+	free(h);
+	return 0;
+}
+
+int group_get_fd(group_handle_t handle)
+{
+	struct group_handle *h = (struct group_handle *) handle;
+	VALIDATE_HANDLE(h);
+	return h->fd;
+}
+
+int group_dispatch(group_handle_t handle)
+{
+	char buf[MAXLINE], *argv[MAXARGS], *act;
+	int argc, rv, i, count, *nodeids;
+	struct group_handle *h = (struct group_handle *) handle;
+	VALIDATE_HANDLE(h);
+
+	memset(buf, 0, sizeof(buf));
+
+	rv = read(h->fd, &buf, sizeof(buf));
+
+	make_args(buf, &argc, argv, ' ');
+	act = argv[0];
+
+	if (!strcmp(act, "stop")) {
+		h->cbs.stop(h, h->private, argv[1]);
+
+	} else if (!strcmp(act, "start")) {
+		count = argc - 4;
+		nodeids = malloc(count * sizeof(int));
+		for (i = 4; i < argc; i++)
+			nodeids[i-4] = atoi(argv[i]);
+		h->cbs.start(h, h->private, argv[1], atoi(argv[2]),
+			     atoi(argv[3]), count, nodeids);
+		free(nodeids);
+
+	} else if (!strcmp(act, "finish")) {
+		h->cbs.finish(h, h->private, argv[1], atoi(argv[2]));
+
+	} else if (!strcmp(act, "terminate")) {
+		h->cbs.terminate(h, h->private, argv[1]);
+
+	} else if (!strcmp(act, "set_id")) {
+		h->cbs.set_id(h, h->private, argv[1], atoi(argv[2]));
+	}
+
+	return 0;
+}
+
---
+++ group/lib/libgroup.h
@@ -0,0 +1,88 @@
+/******************************************************************************
+*******************************************************************************
+**
+**  Copyright (C) 2005 Red Hat, Inc.  All rights reserved.
+**
+**  This library is free software; you can redistribute it and/or
+**  modify it under the terms of the GNU Lesser General Public
+**  License as published by the Free Software Foundation; either
+**  version 2 of the License, or (at your option) any later version.
+**
+**  This library is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+**  Lesser General Public License for more details.
+**
+**  You should have received a copy of the GNU Lesser General Public
+**  License along with this library; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**
+*******************************************************************************
+******************************************************************************/
+
+#ifndef _LIBGROUP_H_
+#define _LIBGROUP_H_
+
+#define MAX_GROUP_MEMBERS	256
+#define MAX_GROUP_NAME_LEN	16
+#define MAX_JOIN_INFO_LEN	16
+
+/* these need to match what's in groupd.h */
+#define GROUP_NODE_FAILED	1
+#define GROUP_NODE_JOIN		2
+#define GROUP_NODE_LEAVE	3
+
+typedef void *group_handle_t;
+
+typedef void (*group_stop_t)(group_handle_t h, void *private, char *name);
+typedef void (*group_start_t)(group_handle_t h, void *private, char *name,
+			      int event_nr, int type, int member_count,
+			      int *members);
+typedef void (*group_finish_t)(group_handle_t h, void *private, char *name,
+			       int event_nr);
+typedef void (*group_terminate_t)(group_handle_t h, void *private, char *name);
+typedef void (*group_set_id_t)(group_handle_t h, void *private, char *name,
+			       int id);
+
+typedef struct {
+	group_stop_t stop;
+	group_start_t start;
+	group_finish_t finish;
+	group_terminate_t terminate;
+	group_set_id_t set_id;
+} group_callbacks_t;
+
+group_handle_t group_init(void *private, char *prog_name, int level, group_callbacks_t *cbs);
+
+int group_exit(group_handle_t handle);
+
+int group_join(group_handle_t handle, char *name, char *info);
+int group_leave(group_handle_t handle, char *name);
+int group_done(group_handle_t handle, char *name, int event_nr);
+int group_get_fd(group_handle_t handle);
+int group_dispatch(group_handle_t handle);
+
+/* int group_join_info(group_handle_t handle, char *name, int event_nr); */
+
+/*
+int group_send();
+int group_receive();
+*/
+
+/*
+int group_get_group_count(group_handle_t handle);
+int group_get_groups(group_handle_t handle, int max, int *count,
+		     group_t *groups);
+int group_get_group(group_handle_t handle, char *name, int level, group_t *g);
+
+typedef struct group {
+	char name[MAX_GROUP_NAME_LEN+1];
+	int joinleave_state;
+	int update_state;
+	int recover_state;
+	int member_count;
+	int members[MAX_GROUP_MEMBERS];
+} group_t;
+*/
+
+#endif
