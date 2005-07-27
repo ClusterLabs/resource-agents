80aac5f5df1095f5ccffeeb29fbf21a1408aa30f
David Teigland <teigland@redhat.com>
2005-07-27 08:42:09 +0000

    get lm_interface.h from ../harness

--- gfs-kernel/src/nolock/main.c
+++ gfs-kernel/src/nolock/main.c
@@ -18,7 +18,8 @@
 #include <linux/types.h>
 #include <linux/fs.h>
 #include <linux/smp_lock.h>
-#include <linux/lm_interface.h>
+
+#include <../harness/lm_interface.h>
 
 struct nolock_lockspace {
 	unsigned int nl_lvb_size;
