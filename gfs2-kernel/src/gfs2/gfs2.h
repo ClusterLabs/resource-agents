97c768d4324309b1bc759ed3b3d342c67f66172a
David Teigland <teigland@redhat.com>
2005-09-06 03:32:16 +0000

    more trivia, replace define with enum

--- gfs2-kernel/src/gfs2/gfs2.h
+++ gfs2-kernel/src/gfs2/gfs2.h
@@ -17,14 +17,20 @@
 #include "incore.h"
 #include "util.h"
 
-#define NO_CREATE 0
-#define CREATE 1
+enum {
+	NO_CREATE = 0,
+	CREATE = 1,
+};
 
-#define NO_WAIT 0
-#define WAIT 1
+enum {
+	NO_WAIT = 0,
+	WAIT = 1,
+};
 
-#define NO_FORCE 0
-#define FORCE 1
+enum {
+	NO_FORCE = 0,
+	FORCE = 1,
+};
 
 /*  Divide num by den.  Round up if there is a remainder.  */
 #define DIV_RU(num, den) (((num) + (den) - 1) / (den))
