87865ec957256248f48dc0cacfd9c125905d4293
Lon Hohberger <lhh@redhat.com>
2005-10-04 20:21:46 +0000

    Clustat partial rewrite (still needs updated XML output)

--- rgmanager/src/utils/clustat.c
+++ rgmanager/src/utils/clustat.c
@@ -7,10 +7,17 @@
 #include <ncurses.h>
 #include <term.h>
 #include <termios.h>
+#include <ccs.h>
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+
+#define FLAG_LOCAL 0x1
+#define FLAG_UP    0x2
+#define FLAG_RGMGR 0x4
+#define FLAG_NOCFG 0x8	/* Shouldn't happen */
+
 
 int running = 1;
 
@@ -101,23 +108,158 @@ rg_state_list(uint64_t local_node_id)
 }
 
 
+cluster_member_list_t *ccs_member_list(void)
+{
+	int desc;
+	int x;
+	char buf[128];
+	char *name;
+	cluster_member_list_t *ret = NULL;
+
+	desc = ccs_connect();
+	if (desc < 0) {
+		return NULL;
+	}
+
+	x = 1;
+	
+	snprintf(buf, sizeof(buf),
+		 "/cluster/clusternodes/clusternode[%d]/@name", x);
+	while (ccs_get(desc, buf, &name) == 0) {
+		if (!ret) {
+			ret = malloc(cml_size(x));
+			if (!ret) {
+				perror("malloc");
+				ccs_disconnect(desc);
+				exit(1);
+			}
+			memset(ret, 0, cml_size(x));
+		} else {
+			ret = realloc(ret, cml_size(x));
+			if (!ret) {
+				perror("realloc");
+				ccs_disconnect(desc);
+				exit(1);
+			}
+		}
+
+		memset(&ret->cml_members[x-1], 0, sizeof(cluster_member_t));
+		strncpy(ret->cml_members[x-1].cm_name, name,
+			sizeof(ret->cml_members[x-1].cm_name));
+		free(name);
+
+		ret->cml_count = x;
+		++x;
+		snprintf(buf, sizeof(buf),
+			 "/cluster/clusternodes/clusternode[%d]/@name", x);
+	}
+
+	ccs_disconnect(desc);
+	return ret;
+}
+
+
+void
+flag_nodes(cluster_member_list_t *all, cluster_member_list_t *these,
+	   uint8_t flag)
+{
+	int x;
+	cluster_member_t *m;
+
+	for (x=0; x<all->cml_count; x++) {
+
+		m = memb_name_to_p(these, all->cml_members[x].cm_name);
+
+		if (m) {
+			all->cml_members[x].cm_id = m->cm_id;
+			all->cml_members[x].cm_state |= flag;
+		}
+	}
+}
+
+
+cluster_member_list_t *
+add_missing(cluster_member_list_t *all, cluster_member_list_t *these)
+{
+	int x, y;
+	cluster_member_t *m, *new;
+
+	for (x=0; x<these->cml_count; x++) {
+
+		m = NULL;
+        	for (y = 0; y < all->cml_count; y++) {
+			if (!strcmp(all->cml_members[y].cm_name,
+				    these->cml_members[x].cm_name))
+                        	m = &all->cml_members[y];
+		}
+
+		if (!m) {
+			printf("%s not found\n", these->cml_members[x].cm_name);
+			/* WTF? It's not in our config */
+			printf("realloc %d\n", (int)cml_size((all->cml_count+1)));
+			all = realloc(all, cml_size((all->cml_count+1)));
+			if (!all) {
+				perror("realloc");
+				exit(1);
+			}
+			
+			new = &all->cml_members[all->cml_count];
+
+			memcpy(new, &these->cml_members[x],
+			       sizeof(cluster_member_t));
+
+			if (new->cm_state == STATE_UP) {
+				new->cm_state = FLAG_UP | FLAG_NOCFG;
+			} else {
+				new->cm_state = FLAG_NOCFG;
+			}
+			++all->cml_count;
+
+		}
+	}
+
+	return all;
+}
+
+
+char *
+my_memb_id_to_name(cluster_member_list_t *members, uint64_t memb_id)
+{
+	int x;
+
+	if (memb_id == NODE_ID_NONE)
+		return "unknown";
+
+	for (x = 0; x < members->cml_count; x++) {
+		if (members->cml_members[x].cm_id == memb_id)
+			return members->cml_members[x].cm_name;
+	}
+
+	return "unknown";
+}
+
 void
 txt_rg_state(rg_state_t *rs, cluster_member_list_t *members)
 {
+	char owner[31];
+
+
 	if (rs->rs_state == RG_STATE_STOPPED ||
 	    rs->rs_state == RG_STATE_DISABLED ||
 	    rs->rs_state == RG_STATE_ERROR ||
 	    rs->rs_state == RG_STATE_FAILED) {
-		printf("  %-20.20s (%-28.28s) %-10.10s\n",
-		       rs->rs_name,
-		       memb_id_to_name(members, rs->rs_last_owner),
-		       rg_state_str(rs->rs_state));
+
+		snprintf(owner, sizeof(owner), "(%-.28s)",
+			 my_memb_id_to_name(members, rs->rs_last_owner));
 	} else {
-		printf("  %-20.20s %-30.30s %-10.10s\n",
-		       rs->rs_name,
-		       memb_id_to_name(members, rs->rs_owner),
-		       rg_state_str(rs->rs_state));
+
+		snprintf(owner, sizeof(owner), "%-.30s",
+			 my_memb_id_to_name(members, rs->rs_owner));
 	}
+	printf("  %-20.20s %-30.30s %-16.16s\n",
+	       rs->rs_name,
+	       owner,
+	       rg_state_str(rs->rs_state));
 }
 
 
@@ -177,16 +319,10 @@ txt_quorum_state(int qs)
 	printf("Member Status: ");
 
 	if (qs & QF_QUORATE)
-		printf("Quorate");
+		printf("Quorate\n\n");
 	else {
-		printf("Inquorate\n");
-		return;
+		printf("Inquorate\n\n");
 	}
-
-	if (qs & QF_GROUPMEMBER)
-		printf(", Group Member");
-
-	printf("\n\n");
 }
 
 
@@ -198,7 +334,7 @@ xml_quorum_state(int qs)
 	if (qs & QF_QUORATE)
 		printf("quorate=\"1\"");
 	else {
-		printf("quorate=\"0\" groupmember-\"0\"/>\n");
+		printf("quorate=\"0\" groupmember=\"0\"/>\n");
 		return;
 	}
 
@@ -212,16 +348,25 @@ xml_quorum_state(int qs)
 void
 txt_member_state(cluster_member_t *node)
 {
-	char *state;
+	printf("  %-40.40s ", node->cm_name);
 
-	if (node->cm_state == STATE_UP)
-		state = "Online";
+	if (node->cm_state & FLAG_UP)
+		printf("Online");
 	else
-		state = "Offline";
+		printf("Offline");
 
-	printf("  %-40.40s %-10s 0x%08x%08x\n", node->cm_name, state,
-	       (uint32_t)((node->cm_id >> 32)&0xffffffff),
-	       (uint32_t)((node->cm_id      )&0xffffffff));
+	if (node->cm_state & FLAG_LOCAL)
+		printf(", Local");
+	
+	if (node->cm_state & FLAG_NOCFG)
+		printf(", Estranged");
+
+	if (node->cm_state & FLAG_RGMGR)
+		printf(", rgmanager");
+
+	printf("\n");
+		
+
 }
 
 
@@ -230,7 +375,7 @@ xml_member_state(cluster_member_t *node)
 {
 	printf("    <node name=\"%s\" state=\"%d\" nodeid=\"0x%08x%08x\"/>\n",
 	       node->cm_name,
-	       node->cm_state,
+	       node->cm_state &0x1,
 	       (uint32_t)((node->cm_id >> 32)&0xffffffff),
 	       (uint32_t)((node->cm_id      )&0xffffffff));
 }
@@ -241,8 +386,8 @@ txt_member_states(cluster_member_list_t *membership)
 {
 	int x;
 
-	printf("  %-40.40s %-10s %s\n", "Member Name", "State", "ID");
-	printf("  %-40.40s %-10s %s\n", "------ ----", "-----", "--");
+	printf("  %-40.40s %s\n", "Member Name", "Status");
+	printf("  %-40.40s %s\n", "------ ----", "------");
 
 	for (x = 0; x < membership->cml_count; x++)
 		txt_member_state(&membership->cml_members[x]);
@@ -273,17 +418,11 @@ txt_cluster_status(int qs, cluster_member_list_t *membership,
 	txt_quorum_state(qs);
 
 	if (!membership || !(qs & QF_GROUPMEMBER)) {
-		printf("Not a member of the Resource Manager service "
-		       "group.\n");
-		printf("Resource Group information unavailable; showing "
-		       "all cluster members.\n\n");
-		membership = clu_member_list(NULL);
-		txt_member_states(membership);
-		cml_free(membership);
-	} else {
-		txt_member_states(membership);
+		printf("Resource Group Manager not running; no service "
+		       "information available.\n\n");
 	}
 
+	txt_member_states(membership);
 	txt_rg_states(rgs, membership);
 }
 
@@ -299,6 +438,67 @@ xml_cluster_status(int qs, cluster_member_list_t *membership,
 	if (rgs)
 		xml_rg_states(rgs, membership);
 	printf("</clustat>\n");
+}
+
+
+void
+dump_node(cluster_member_t *node)
+{
+	printf("Node %s state %02x\n", node->cm_name, node->cm_state);
+}
+
+
+void 
+dump_nodes(cluster_member_list_t *nodes)
+{
+	int x;
+
+	for (x=0; x<nodes->cml_count; x++) {
+		dump_node(&nodes->cml_members[x]);
+	}
+}
+
+
+
+cluster_member_list_t *
+build_member_list(uint64_t *lid)
+{
+	cluster_member_list_t *all, *part;
+	cluster_member_t *m;
+	int x;
+
+	/* Get all members from ccs, and all members reported by the cluster
+	   infrastructure */
+	all = ccs_member_list();
+	part = clu_member_list(NULL);
+	msg_update(part); /* XXX magmamsg is awful. */
+
+	/* Flag online nodes */
+	flag_nodes(all, part, FLAG_UP);
+
+	/* See if our config has anyone missed.  If so, flag them as missing 
+	   from the config file */
+	all = add_missing(all, part);
+
+	/* Grab the local node ID and flag it from the list of reported
+	   online nodes */
+	clu_local_nodeid(NULL, lid);
+	for (x=0; x<all->cml_count; x++) {
+		if (all->cml_members[x].cm_id == *lid) {
+			m = &all->cml_members[x];
+			m->cm_state |= FLAG_LOCAL;
+			break;
+		}
+	}
+			
+	cml_free(part);
+
+	/* Flag rgmanager nodes, if any */
+	part = clu_member_list(RG_SERVICE_GROUP);
+	flag_nodes(all, part, FLAG_RGMGR);
+	cml_free(part);
+
+	return all;
 }
 
 
@@ -403,9 +603,8 @@ main(int argc, char **argv)
 
 	while (1) {
 		qs = clu_quorum_status(RG_SERVICE_GROUP);
-		clu_local_nodeid(RG_SERVICE_GROUP, &local_node_id);
-		membership = clu_member_list(RG_SERVICE_GROUP);
-		msg_update(membership);
+		membership = build_member_list(&local_node_id);
+		
 		rgs = rg_state_list(local_node_id);
 
 		if (refresh_sec) {
