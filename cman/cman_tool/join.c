b523b9a1b8fe9814b1a6dfc3a9d80a50272c413c
Patrick Caulfield <pcaulfie@redhat.com>
2006-07-03 07:58:39 +0000

    Run aisexec from SBINDIR

--- cman/cman_tool/join.c
+++ cman/cman_tool/join.c
@@ -128,9 +128,9 @@ int join(commandline_t *comline)
 
 	case 0: // child
 		be_daemon(!comline->verbose);
-		chdir(LIBEXECDIR);
+		chdir(SBINDIR);
 		execve("./aisexec", argv, envp);
-		die("execve of " LIBEXECDIR "/aisexec failed: %s", strerror(errno));
+		die("execve of " SBINDIR "/aisexec failed: %s", strerror(errno));
 		break;
 
 	default: //parent
