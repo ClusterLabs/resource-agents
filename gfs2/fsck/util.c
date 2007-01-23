aa18d9ba8fa3f38fa17cf04c39a1ae13502b6f34
Robert Peterson <rpeterso@redhat.com>
2007-01-23 20:55:46 +0000

    Resolves: bz 223500: gfs2_fsck runs slower than previous version

--- gfs2/fsck/util.c
+++ gfs2/fsck/util.c
@@ -58,20 +58,25 @@ int compute_height(struct gfs2_sbd *sdp, uint64_t sz)
 /* doesn't think we hung.  (This may take a long time).       */
 void warm_fuzzy_stuff(uint64_t block)
 {
+	static uint64_t one_percent = 0;
 	static struct timeval tv;
 	static uint32_t seconds = 0;
 	
-	last_reported_block = block;
-	gettimeofday(&tv, NULL);
-	if (!seconds)
-		seconds = tv.tv_sec;
-	if (tv.tv_sec - seconds) {
-		uint64_t percent;
+	if (!one_percent)
+		one_percent = last_fs_block / 100;
+	if (block - last_reported_block >= one_percent) {
+		last_reported_block = block;
+		gettimeofday(&tv, NULL);
+		if (!seconds)
+			seconds = tv.tv_sec;
+		if (tv.tv_sec - seconds) {
+			static uint64_t percent;
 
-		seconds = tv.tv_sec;
-		if (last_fs_block) {
-			percent = (block * 100) / last_fs_block;
-			log_notice("\r%" PRIu64 " percent complete.\r", percent);
+			seconds = tv.tv_sec;
+			if (last_fs_block) {
+				percent = (block * 100) / last_fs_block;
+				log_notice("\r%" PRIu64 " percent complete.\r", percent);
+			}
 		}
 	}
 }
