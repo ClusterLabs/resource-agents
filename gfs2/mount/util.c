83a4d71f2bdee5fa4382694b5fd128ca5a4c8dfe
Robert Peterson <rpeterso@redhat.com>
2007-05-11 20:04:30 +0000

    Resolves: Bugzilla Bug 239844: mount.gfs2 doesn't work when _netdev is used in /etc/fstab.

--- gfs2/mount/util.c
+++ gfs2/mount/util.c
@@ -58,6 +58,7 @@ static struct opt_map opt_map[] = {
   { "nouser",   0, 1, 0  },             /* Forbid ordinary user to mount */
   { "owner",    0, 0, 0  },             /* Let the owner of the device mount */
   { "noowner",  0, 1, 0  },             /* Device owner has no special privs */
+  { "_netdev",  0, 0, 0  },             /* Network device required (netfs) */
   { NULL,       0, 0, 0	 }
 };
 
