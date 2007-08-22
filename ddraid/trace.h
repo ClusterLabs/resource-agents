58aaf38070486a44c14f248aa65fe5ca76326845
Fabio M. Di Nitto <fabbione@ubuntu.com>
2007-08-22 18:00:20 +0000

    Remove ddraid from CVS HEAD.
    ACK on cluster-devel and IRC #linux-cluster

--- ddraid/trace.h
+++
@@ -1,7 +0,0 @@
-#define BREAK asm("int3")
-#define warn(string, args...) do { fprintf(stderr, "[%u] %s: " string "\n", getpid(), __func__, ##args); } while (0)
-#define error(string, args...) do { warn(string, ##args); BREAK; } while (0)
-#define assert(expr) do { if (!(expr)) error("Failed assertion \"%s\"\n", #expr); } while (0)
-
-#define trace_on(args) args
-#define trace_off(args)
