8e9c5de693f76df99f6066f6390a030319720ae5
Fabio M. Di Nitto <fabbione@ubuntu.com>
2007-12-21 18:37:16 +0000

    Collaps all man Makefile's common snippets into man/man.mk
    Change all man Makefile's to use it.
    
    From now on it will be enough to source make/man.mk and add a TARGET=
    with the man page.

---
+++ make/man.mk
@@ -0,0 +1,16 @@
+install:
+	for i in ${TARGET}; do \
+	  p=`echo $$i | sed -e 's#.*\.##g'`; \
+	  install -d ${mandir}/man$$p; \
+	  install $(S)/$$i ${mandir}/man$$p; \
+	done
+
+uninstall:
+	for i in ${TARGET}; do \
+	  p=`echo $$i | sed -e 's#.*\.##g'`; \
+	  ${UNINSTALL} $$i ${mandir}/man$$p; \
+	done
+
+all:
+
+clean:
