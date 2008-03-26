abf5d4d8a61b28ee786b94436c6f82ee484f7fc7
Fabio M. Di Nitto <fabbione@fabbione.net>
2008-03-26 07:25:36 +0100

    [BUILD] Fix man page install permission
    Man pages were installed -m755 that is wrong. Install them
    as -m644 as it should be.
    
    Signed-off-by: Fabio M. Di Nitto <fabbione@fabbione.net>

--- make/man.mk
+++ make/man.mk
@@ -4,7 +4,7 @@ install:
 	for i in ${TARGET}; do \
 	  p=`echo $$i | sed -e 's#.*\.##g'`; \
 	  install -d ${mandir}/man$$p; \
-	  install $(S)/$$i ${mandir}/man$$p; \
+	  install -m644 $(S)/$$i ${mandir}/man$$p; \
 	done
 
 uninstall:
