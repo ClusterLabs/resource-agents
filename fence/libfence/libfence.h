d1d29545d7efeefbe4cd540bdd3dfd248d2310c0
Ryan McCabe <rmccabe@redhat.com>
2008-05-28 16:30:42 -0400

    libfence: update copyright notice
    - Clarify copyright notice so that GPLv2 is specified explicitly.

--- fence/libfence/libfence.h
+++ fence/libfence/libfence.h
@@ -4,9 +4,8 @@
 **  Copyright (C) 2008 Red Hat, Inc.  All rights reserved.
 **
 **  This library is free software; you can redistribute it and/or
-**  modify it under the terms of the GNU Lesser General Public
-**  License as published by the Free Software Foundation; either
-**  version 2 of the License, or (at your option) any later version.
+**  modify it under the terms of version 2 of the GNU Lesser General
+**  Public License as published by the Free Software Foundation.
 **
 **  This library is distributed in the hope that it will be useful,
 **  but WITHOUT ANY WARRANTY; without even the implied warranty of
