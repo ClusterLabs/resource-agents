8c4044a4131dbf70a0f276fdb7b5261f1e37d980
Fabio M. Di Nitto <fdinitto@redhat.com>
2008-06-12 09:36:16 +0200

    [QDISK] Add better support for Xen virtual block devices
    This change allows to detect Xen virtual disks directly
    into sysfsattrs.disk without the need of an external filter.
    
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- cman/qdisk/scandisk.c
+++ cman/qdisk/scandisk.c
@@ -580,7 +580,17 @@ static int sysfs_is_disk(char *path)
 
 	snprintf(newpath, sizeof(newpath), "%s/../device/media", path);
 	if (lstat(newpath, &sb))
-		return -1;
+		goto found;
+
+	snprintf(newpath, sizeof(newpath), "%s/device/devtype", path);
+	if (lstat(newpath, &sb))
+		return 1;
+
+	snprintf(newpath, sizeof(newpath), "%s/../device/devtype", path);
+	if (lstat(newpath, &sb))
+		return 1;
+
+	return -1;
 
       found:
 	f = fopen(newpath, "r");
