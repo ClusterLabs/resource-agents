cb690444146f443739976b8795407545666addb1
Fabio M. Di Nitto <fdinitto@redhat.com>
2008-06-20 07:31:36 +0200

    [CCS] Shrink more common code for internal xml queries
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- ccs/daemon/misc.c
+++ ccs/daemon/misc.c
@@ -32,122 +32,6 @@ open_doc_t *master_doc = NULL;
 
 LOGSYS_DECLARE_SUBSYS ("CCS", LOG_INFO);
 
-int get_doc_version(xmlDocPtr ldoc){
-  int i;
-  int error = 0;
-  xmlXPathObjectPtr  obj = NULL;
-  xmlXPathContextPtr ctx = NULL;
-  xmlNodePtr        node = NULL;
-
-  CCSENTER("get_doc_version");
-
-  ctx = xmlXPathNewContext(ldoc);
-  if(!ctx){
-    log_printf(LOG_ERR, "Error: unable to create new XPath context.\n");
-    error = -EIO;  /* ATTENTION -- what should this be? */
-    goto fail;
-  }
-
-  obj = xmlXPathEvalExpression((xmlChar *)"/cluster/@config_version", ctx);
-  if(!obj || !obj->nodesetval || (obj->nodesetval->nodeNr != 1)){
-    log_printf(LOG_ERR, "Error while retrieving config_version.\n");
-    error = -ENODATA;
-    goto fail;
-  }
-
-  node = obj->nodesetval->nodeTab[0];
-  if(node->type != XML_ATTRIBUTE_NODE){
-    log_printf(LOG_ERR, "Object returned is not of attribute type.\n");
-    error = -ENODATA;
-    goto fail;
-  }
-
-  if(!node->children->content || !strlen((char *)node->children->content)){
-    log_printf(LOG_DEBUG, "No content found.\n");
-    error = -ENODATA;
-    goto fail;
-  }
-  
-  for(i=0; i < strlen((char *)node->children->content); i++){
-    if(!isdigit(node->children->content[i])){
-      log_printf(LOG_ERR, "config_version is not a valid integer.\n");
-      error = -EINVAL;
-      goto fail;
-    }
-  }
-  error = atoi((char *)node->children->content);
-
-fail:
-
-  if(ctx){
-    xmlXPathFreeContext(ctx);
-  }
-  if(obj){
-    xmlXPathFreeObject(obj);
-  }
-  CCSEXIT("get_doc_version");
-  return error;
-}
-
-
-/**
- * get_cluster_name
- * @ldoc:
- *
- * The caller must remember to free the string that is returned.
- *
- * Returns: NULL on failure, (char *) otherwise
- */
-char *get_cluster_name(xmlDocPtr ldoc){
-  int error = 0;
-  char *rtn = NULL;
-  xmlXPathObjectPtr  obj = NULL;
-  xmlXPathContextPtr ctx = NULL;
-  xmlNodePtr        node = NULL;
-
-  CCSENTER("get_cluster_name");
-
-  ctx = xmlXPathNewContext(ldoc);
-  if(!ctx){
-    log_printf(LOG_ERR, "Error: unable to create new XPath context.\n");
-    error = -EIO;  /* ATTENTION -- what should this be? */
-    goto fail;
-  }
-
-  obj = xmlXPathEvalExpression((xmlChar *)"/cluster/@name", ctx);
-  if(!obj || !obj->nodesetval || (obj->nodesetval->nodeNr != 1)){
-    log_printf(LOG_ERR, "Error while retrieving config_version.\n");
-    error = -ENODATA;
-    goto fail;
-  }
-
-  node = obj->nodesetval->nodeTab[0];
-  if(node->type != XML_ATTRIBUTE_NODE){
-    log_printf(LOG_ERR, "Object returned is not of attribute type.\n");
-    error = -ENODATA;
-    goto fail;
-  }
-
-  if(!node->children->content || !strlen((char *)node->children->content)){
-    log_printf(LOG_DEBUG, "No content found.\n");
-    error = -ENODATA;
-    goto fail;
-  }
-
-  rtn = strdup((char *)node->children->content);
-
-fail:
-
-  if(ctx){
-    xmlXPathFreeContext(ctx);
-  }
-  if(obj){
-    xmlXPathFreeObject(obj);
-  }
-  CCSEXIT("get_cluster_name");
-  return rtn;
-}
-
 /**
  * do_simple_xml_query
  * @ctx: xml context
@@ -178,6 +62,81 @@ static char *do_simple_xml_query(xmlXPathContextPtr ctx, char *query) {
     xmlXPathFreeObject(obj);
 
   return NULL;
+}
+
+int get_doc_version(xmlDocPtr ldoc){
+  int i;
+  int error = 0;
+  xmlXPathContextPtr ctx = NULL;
+  char *res = NULL;
+
+  CCSENTER("get_doc_version");
+
+  ctx = xmlXPathNewContext(ldoc);
+  if(!ctx){
+    log_printf(LOG_ERR, "Error: unable to create new XPath context.\n");
+    error = -EIO;  /* ATTENTION -- what should this be? */
+    goto fail;
+  }
+
+  res =  do_simple_xml_query(ctx, "/cluster/@config_version");
+  if(res) {
+    for(i=0; i < strlen(res); i++){
+      if(!isdigit(res[i])){
+        log_printf(LOG_ERR, "config_version is not a valid integer.\n");
+        error = -EINVAL;
+        goto fail;
+      }
+    }
+    error = atoi(res);
+  } else
+    error = -EINVAL;
+
+fail:
+
+  if(res)
+	free(res);
+
+  if(ctx){
+    xmlXPathFreeContext(ctx);
+  }
+
+  CCSEXIT("get_doc_version");
+  return error;
+}
+
+
+/**
+ * get_cluster_name
+ * @ldoc:
+ *
+ * The caller must remember to free the string that is returned.
+ *
+ * Returns: NULL on failure, (char *) otherwise
+ */
+char *get_cluster_name(xmlDocPtr ldoc){
+  int error = 0;
+  char *rtn = NULL;
+  xmlXPathContextPtr ctx = NULL;
+
+  CCSENTER("get_cluster_name");
+
+  ctx = xmlXPathNewContext(ldoc);
+  if(!ctx){
+    log_printf(LOG_ERR, "Error: unable to create new XPath context.\n");
+    error = -EIO;  /* ATTENTION -- what should this be? */
+    goto fail;
+  }
+
+  rtn = do_simple_xml_query(ctx, "/cluster/@name");
+
+fail:
+
+  if(ctx){
+    xmlXPathFreeContext(ctx);
+  }
+  CCSEXIT("get_cluster_name");
+  return rtn;
 }
 
 /**
