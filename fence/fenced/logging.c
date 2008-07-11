0dcc5e557008df4d51aef891ebfb998a34785f04
David Teigland <teigland@redhat.com>
2008-07-11 12:04:26 -0500

    fenced: tune logsys settings
    put errors in fenced.log (in addition to syslog) by default
    keep debug out of syslog by default (commented out pending openais commit)
    
    Signed-off-by: David Teigland <teigland@redhat.com>

--- fence/fenced/logging.c
+++ fence/fenced/logging.c
@@ -1,6 +1,12 @@
 #include "fd.h"
 
-#define DEFAULT_MODE		LOG_MODE_OUTPUT_SYSLOG_THREADED
+/* default: errors go to syslog (/var/log/messages) and fenced.log
+   logging/debug=on: errors continue going to syslog (/var/log/messages)
+   and fenced.log, debug messages are added to fenced.log. */
+
+#define DEFAULT_MODE		LOG_MODE_OUTPUT_SYSLOG_THREADED | \
+				LOG_MODE_NOSUBSYS | \
+				LOG_MODE_OUTPUT_FILE /* | LOG_MODE_NO_DEBUG_SYSLOG */
 #define DEFAULT_FACILITY	SYSLOGFACILITY /* cluster config setting */
 #define DEFAULT_PRIORITY	SYSLOGLEVEL /* cluster config setting */
 #define DEFAULT_FILE		LOGDIR "/fenced.log"
