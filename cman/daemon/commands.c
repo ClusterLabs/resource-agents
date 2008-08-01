e5885fd588a0608ddf00ad03ee3289a1b0c9f656
Christine Caulfield <ccaulfie@redhat.com>
2008-08-01 10:14:31 +0100

    cman: Revert dirty patch
    Revert patch 288ab73e51f51ce174f51dc2fc67c6dd1fe03e85 as it's
    dangerously wrong and based on several misunderstandings.
    
    see bz#443358 for mroe information
    
    Signed-off-by: Christine Caulfield <ccaulfie@redhat.com>

--- cman/daemon/commands.c
+++ cman/daemon/commands.c
@@ -1778,8 +1778,7 @@ static void do_process_transition(int nodeid, char *data, int len)
 
 	/* Newer nodes 6.1.0 onwards, set the DIRTY flag if they have state. If the new node has been down
 	   and has state then we mark it disallowed because we cannot merge stateful nodes */
-	if (msg->flags & NODE_FLAGS_DIRTY && (node->flags & NODE_FLAGS_BEENDOWN ||
-	    (us->flags & NODE_FLAGS_DIRTY && nodeid != us->node_id))) {
+	if (msg->flags & NODE_FLAGS_DIRTY && (node->flags & NODE_FLAGS_BEENDOWN)) {
 		/* Don't duplicate messages */
 		if (node->state != NODESTATE_AISONLY) {
 			if (cluster_is_quorate) {
