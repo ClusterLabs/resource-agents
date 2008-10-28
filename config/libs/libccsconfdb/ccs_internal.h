503560cbd3e295159bf947b09de4c878e3c5ce81
Fabio M. Di Nitto <fdinitto@redhat.com>
2008-10-28 20:16:40 +0100

    ccs: remove duplicate entry in internal header file
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- config/libs/libccsconfdb/ccs_internal.h
+++ config/libs/libccsconfdb/ccs_internal.h
@@ -3,9 +3,6 @@
 
 /* NOTE: use __attribute__ to hide the internal API */
 
-/* from xpathlite.c */
-char * _ccs_get_xpathlite(confdb_handle_t handle, unsigned int connection_handle, const char *query, int list) __attribute__ ((visibility ("hidden")));
-
 /* from libccs.c */
 void reset_iterator(confdb_handle_t handle, unsigned int connection_handle) __attribute__ ((visibility ("hidden")));
 int get_previous_query(confdb_handle_t handle, unsigned int connection_handle, char *previous_query, unsigned int *query_handle) __attribute__ ((visibility ("hidden")));
