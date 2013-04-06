906932a72cb196303be57923bdc45ee574d2558c
David Vossel <dvossel@redhat.com>
2013-04-05 19:39:51 -0500

    High: LVM: Merge redhat lvm.sh into heartbeat LVM agent
    = Overview =
    This is a part of a project aimed at reducing duplicate redhat
    and heartbeat agent functionality.
    
    = New Features=
    This patch leaves the original LVM heartbeat functionality
    intact while adding these addition features from the redhat agent.
    
    1. Exclusive activation using volume group tags.  This feature
    allows a volume group to live on shared storage within the cluster
    without requiring the use of cLVM for metadata locking.
    
    2. individual logical volume activation for local and cluster
    volume groups by using the new 'lvname' option.
    
    3. Better setup validation when the 'exclusive' option is enabled.
    This patch validates that when exclusive activation is enabled, either
    a cluster volume group is in use with cLVM, or the tags variant is
    configured correctly.  These new checks also makes it impossible to
    enable the exclusive activation for cloned resources.
    
    = Moved Functionality =
    Functionality from the original heartbeat agent has moved into new
    function names and files. This is a map to understanding where the
    original functionality lives within the new merged agent.
    
    - LVM_monitor and LVM_status --> vg_status_normal() in lvm_vg.sh
      The new monitor function does additional checking to verify
      both the volume group and each logical volume is correctly activated.
    
    - LVM_start() ---> vg_start_normal() in lvm_vg.sh
    
    - LVM_stop()  ---> vg_stop_normal() in lvm_vg.sh
    
    - LVM_validate_all() ---> verify_setup() in lvm_functions.sh
      The new function also does the new exclusive activation setup
      validation checks.

---
+++ heartbeat/member_util.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+# Copyright (C) 1997-2003 Sistina Software, Inc.  All rights reserved.
+# Copyright (C) 2004-2011 Red Hat, Inc.  All rights reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+
+#
+# Use corosync-quorumtool to figure out if the specified node is a member
+# of the cluster.  Returns 1 if not a member, and
+# 0 if the node is happily running.
+#
+# Tested on RHEL6 and F17  Note that the old version of this function utilized
+# clustat, which had introspection in to the configuration.
+# If a node was not found, the old version would return '2', but the only
+# consumer of this function never cared about that value.
+#
+is_node_member_clustat()
+{
+	# Still having a tag while (a) online but (b) not running pacemaker 
+	# (e.g. crm_node) or rgmanager not considered adequate for things like
+	# the LVM agent - so we use corosync-quorumtool instead.  The function
+	# name really should be changed.
+	#
+	# corosync 1.4.1 output looks like:
+	#
+	#  # corosync-quorumtool  -l
+	#  Nodeid     Name
+	#     1   rhel6-1
+	#     2   rhel6-2
+	#
+	# corosync 2.0.1 output looks like:
+	#  # corosync-quorumtool -l
+	#
+	#  Membership information
+	#  ----------------------
+	#      Nodeid      Votes Name
+	#           1          1 rhel7-1.priv.redhat.com
+	#           2          1 rhel7-2.priv.redhat.com
+	#
+	corosync-quorumtool -l | grep -v "^Nodeid" | grep -i " $1\$" &> /dev/null
+	return $?
+}
+
+
+#
+# Print the local node name to stdout
+# Returns 0 if could be found, 1 if not
+# Tested on RHEL6 (cman) and Fedora 17 (corosync/pacemaker)
+#
+local_node_name()
+{
+	local node nid localid
+
+	if which magma_tool &> /dev/null; then
+		# Use magma_tool, if available.
+		line=$(magma_tool localname | grep "^Local")
+
+		if [ -n "$line" ]; then
+			echo ${line/* = /}
+			return 0
+		fi
+	fi
+
+	if which cman_tool &> /dev/null; then
+		# Use cman_tool
+
+		line=$(cman_tool status | grep -i "Node name: $1")
+		[ -n "$line" ] || return 1
+		echo ${line/*name: /}
+		return 0
+	fi
+
+	if ! which crm_node &> /dev/null; then
+		# no crm_node? :(
+		return 2
+	fi
+
+	localid=$(crm_node -i)
+	crm_node -l | while read nid node; do
+		if [ "$nid" = "$localid" ]; then
+			echo $node
+			return 0
+		fi
+	done
+
+	return 1
+}
+
