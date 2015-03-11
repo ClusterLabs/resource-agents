ad84db19512dc9004f78466507617ac196323f25
Takayuki Nagata <tnagata@redhat.com>
2015-03-11 16:32:33 +0900

    orainstance.sh: Fix process name grep in exit_idle
    Fix a bug that can cause the script to pause for 90 seconds.
    rhbz#1200639

--- rgmanager/src/resources/orainstance.sh
+++ rgmanager/src/resources/orainstance.sh
@@ -216,7 +216,7 @@ exit_idle() {
 	declare -i n=0
 	
 	ocf_log debug "Waiting for Oracle processes for $ORACLE_SID to terminate..."
-	while ps ax | grep $ORACLE_SID | grep -v grep | grep -q -v $LSNR_PROCNAME; do
+	while ps ax | grep ora_.*_${ORACLE_SID} | grep -v grep | grep -q -v $LSNR_PROCNAME; do
 		if [ $n -ge 90 ]; then
 			ocf_log debug "Timed out while waiting for Oracle processes for $ORACLE_SID to terminate"
 			force_cleanup
