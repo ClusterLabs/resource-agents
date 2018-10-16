e72f4152077005856026b8eee7c802501881d020
Oyvind Albrigtsen <oalbrigt@redhat.com>
2018-10-16 17:00:19 +0200

    build: dont install systemd-tmpfiles as part of "make release"

--- make/release.mk
+++ make/release.mk
@@ -35,7 +35,7 @@ endif
 tarballs: tag
 	./autogen.sh
 	./configure
-	DISTCHECK_CONFIGURE_FLAGS="--with-systemdsystemunitdir=no" make distcheck
+	DISTCHECK_CONFIGURE_FLAGS="--with-systemdsystemunitdir=no --with-systemdtmpfilesdir=no" make distcheck
 
 sha256: tarballs $(project)-$(version).sha256
 
